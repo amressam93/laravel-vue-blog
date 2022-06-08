45eaaabbb1392e3b22553457c2b18bf139f6d8f0
Amr Essam <79043600+amressam93@users.noreply.github.com>
2022-06-08 13:29:09 +0200

    finishing models and make migration

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -5,18 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Setting extends Model
 {
     use HasFactory;
 
     public $guarded = [''];
-
-
-    public function posts()
-    {
-        return $this->hasMany(Post::class,'category_id');
-    }
-
 
 
 }
